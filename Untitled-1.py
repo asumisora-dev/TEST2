4e6b5f64ec23de650fb805dad4d0f0b54c01ec53
Asumi Sora <asumi_sora@jptechnora.com>
2021-01-12 00:19:05 +0900

    テスト１

---
+++ Untitled-1.py
@@ -0,0 +1,14 @@
+number = 1
+number2 = 1.0
+getattr = 'こんにちは'
+is_ok = True
+
+print(number, type(number))
+print(number2, type(number2))
+print(getattr, type(getattr))
+print(is_ok, type(is_ok))
+
+print(2 > 1)
+print(1 < 1)
+print(3 > 4)
+print(5 * 2)
